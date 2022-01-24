cc62a9575df95ac85a0c26d878e9a6d8b38e9ed7
Antibollo <69866962+sashayakovkev32@users.noreply.github.com>
2022-01-24 16:22:19 +0300

    Update Виселица(игра).py

--- Виселица(игра).py
+++ Виселица(игра).py
@@ -1,29 +1,30 @@
 #Виселица
 import random
 
-print('Приветсвую тебя мой дорогой друг. Давай сыграем','\n')
+words = ['саша','дмитрий','алексей','анатолий']
+zhizni = 3
+secret_word = random.choice(words)
+s = "_" * len(secret_word)
+r = "*" * len(secret_word)
+print('Загадано слово:',r)
 
-my_message = ['саша','дима']
-d = random.choice(my_message)
-zhizn = len(d)
-vvod = ""
-otvet = ""
+# Основной цикл
 
-while zhizn != 0:
-    guest_word = input('введите букву:')
-    for i in guest_word:
-        if i not in d:
-            zhizn -= 1
-            print('не правильная буква,осталось',zhizn,'попытки','\n')
+while zhizni != 0:
+    print("\nОтгаданное вами в слове сейчас выглядит так:\n", s,'\n')
+    guest_letter = input('введите букву:')
+    if guest_letter in secret_word:         # проверка буквы в загад.слове
+        print("Буква есть в слове")
+        otvet = ''
+        for i in range(len(secret_word)):   #
+            if guest_letter == secret_word[i]:
+                otvet += guest_letter
+            else:
+                otvet += s[i]
+        s = otvet
+    else:
+        zhizni -= 1
+        print("Буквы нет в слове,Осталось",zhizni,"жизней",'\n')
 
-        if i in d:
-            print('буква есть в слове','\n')
-            otvet += i
-            print(otvet)
-
-    if otvet == d:
-        print('ты выиграл,правильный ответ:',otvet)
-        break
-
-    if zhizn == 0:
-        print('ты проиграл,правильное слово:',d)
+    if zhizni == 0:
+        print('Ты проиграл,загаданоое слово :',secret_word)
